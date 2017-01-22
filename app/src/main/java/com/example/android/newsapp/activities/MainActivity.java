7865446add44e435c61aa8c42d0868947bd0a2f8
jennifernghinguyen <jennifernghi@yahoo.com>
2017-01-22 14:57:50 -0800

    fixing ui

--- app/src/main/java/com/example/android/newsapp/activities/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.newsapp.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.android.newsapp.R;
 import com.example.android.newsapp.adapters.ViewPagerAdapter;
@@ -25,8 +28,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
         Log.i(LOG_TAG, "in onCreate");
 
         ViewHolder viewHolder = new ViewHolder();
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         //viewHolder.viewPager.addOnPageChangeListener(pageChangeListener);
         viewHolder.tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         viewHolder.tabLayout.setupWithViewPager(viewHolder.viewPager);
+
+
 
 
     }
